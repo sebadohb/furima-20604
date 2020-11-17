79997a9ace77109101ed460f7b5a4af940d59947
sebadohb <sebadohb1986@yahoo.co.jp>
2020-11-17 15:55:52 +0900

    フォームオブジェクトの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,37 @@
 class OrdersController < ApplicationController
   def index
-    
+    @item = Item.find(params[:item_id])
+    @customer = Customer.new
   end
+
+  def new
+  end
+  def create
+    @customer = Customer.new(customer_params)
+    @item = Item.find(params[:item_id])
+    if @customer.valid?
+      pay_item
+      @customer.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:post_code, :city, :prefecture_id, :house_number, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: customer_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,14 +1,14 @@
 class Order < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  with_options presence: true do
-    validates :post_code
-    validates :city
-    validates :house_number
-    validates :phone_number
-  end
-  validates :prefecture_id,  numericality: { other_than: 1, message: 'should be selected' }
-  validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'を入力してください' }
-  validates :phone_number, format: { with: /\A\d{10}\z/, message: 'を入力してください' }
-  belongs_to purchase
+  # with_options presence: true do
+  #   validates :post_code
+  #   validates :city
+  #   validates :house_number
+  #   validates :phone_number
+  # end
+  # validates :prefecture_id,  numericality: { other_than: 1, message: 'should be selected' }
+  # validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'を入力してください' }
+  # validates :phone_number, format: { with: /\A\d{10}\z/, message: 'を入力してください' }
+  belongs_to :purchase
   belongs_to_active_hash :prefecture
 end
